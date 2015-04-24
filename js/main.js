ca07d5e4070acb64b3bcc32028a95b8d9e025be5
Ryan Closner <me@rynclsnr.com>
2015-04-23 21:49:05 -0400

    BGBGBGBG Ch-ch-changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 (function () {
-  // NOTHING HERE ¯\_(ツ)_/¯
+  var size = 120;
+  var $body = $('body');
+
+  $body.css('background-image', 'url("img/me.jpg")');
+
+  setInterval(function () {
+    $body.css('background-size', size + 'px');
+
+    if (size <= 1) {
+      size = 120;
+    } else {
+      size--;
+    }
+
+  }, 200);
 })();
